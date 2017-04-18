1e6e49c1d4fa03d9867d7715247c9f6264ab8a73
Alvaro Flores <alvaro.flores01@gmail.com>
2017-04-17 20:04:29 -0700

    adds contact form backend and fixes margin issues

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
-function rotate() {
-  var element = document.getElementsByClassName('jupiter');
-  var position= 0;
-  var id set
-}
+// function rotate() {
+//   var element = document.getElementsByClassName('jupiter');
+//   var position= 0;
+//   var id set
+// }
+
+$(document).ready(function() {
+  $("#submit").click(function() {
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+  });
+});
