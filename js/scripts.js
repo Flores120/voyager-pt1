41a5d94544a6752366e88d47f2072fadff0eb4b8
Alvaro Flores <alvaro.flores01@gmail.com>
2017-05-16 12:07:57 -0700

    adds a modal for first time users

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
+  $('#alert-click').show(3000);
     $("#menu").click(function(){
       $("#menu-list").toggle(500);
+    });
+    $("#closebtn").click(function(){
+      $('#alert-click').hide(1000);
     });
 });
